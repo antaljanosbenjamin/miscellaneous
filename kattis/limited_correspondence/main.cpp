d5226b8c5fd5bfd177c312d5361707e3213b8b07
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2019-08-16 21:53:14 +0200

    Add base solution

---
+++ kattis/limited_correspondence/main.cpp
@@ -0,0 +1,17 @@
+// https://open.kattis.com/submissions/4374599
+
+#include "LetterAssembler.hpp"
+#include <fstream>
+#include <iostream>
+
+int main() {
+  auto caseNumber = 1u;
+
+  while (std::cin.good() == true) {
+    LetterAssembler letterAssembler;
+    if (letterAssembler.read(std::cin)) {
+      std::cout << "Case " << caseNumber++ << ": " << letterAssembler.assemble() << std::endl;
+    }
+  }
+  return 0;
+}
