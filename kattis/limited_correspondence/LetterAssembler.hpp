fce9fc850b1d393368d57ed2a60391fdc06ed6e8
Antal János Benjamin <antal.janos.benjamin@gmail.com>
2019-08-17 17:04:07 +0200

    Beautify

--- kattis/limited_correspondence/LetterAssembler.hpp
+++ kattis/limited_correspondence/LetterAssembler.hpp
@@ -1,13 +1,10 @@
-#pragma once
+﻿#pragma once
 
-#include <algorithm>
 #include <iostream>
-#include <set>
-#include <tuple>
 #include <vector>
 
-struct LetterAssembler {
-
+class LetterAssembler {
+public:
   struct AssemblerState {
     std::vector<bool> used;
     std::string firstLetter;
@@ -15,90 +12,16 @@ struct LetterAssembler {
     std::string best;
   };
 
-  bool read(std::istream &inputStream) {
-    auto pairCount = 0u;
-    auto sumFirstWordLength = 0;
-    auto sumSecondWordLength = 0;
-    this->wordPairs.clear();
+  void read(std::istream &inputStream);
 
-    inputStream >> pairCount;
+  std::string assemble();
 
-    if (pairCount == 0){
-      return false;
-    }
-
-    this->wordPairs.reserve(pairCount);
-
-    for (size_t pos = 0; pos < pairCount; ++pos) {
-      std::string firstWord;
-      std::string secondWord;
-
-      inputStream >> firstWord >> secondWord;
-
-      sumFirstWordLength += firstWord.length();
-      sumSecondWordLength += secondWord.length();
-
-      this->wordPairs.push_back(std::make_pair(std::move(firstWord), std::move(secondWord)));
-    }
-
-    inputStream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-    std::sort(this->wordPairs.begin(), wordPairs.end());
-    this->maximumPossibleLength = std::min(sumFirstWordLength, sumSecondWordLength);
-
-    return true;
-  }
-
-  std::string assemble() {
-    AssemblerState state;
-    state.firstLetter.reserve(maximumPossibleLength);
-    state.secondLetter.reserve(maximumPossibleLength);
-    state.used = std::vector<bool>(this->wordPairs.size(), false);
-    this->recursiveSearch(state);
-    if (state.best.empty()) {
-      return "IMPOSSIBLE";
-    } else {
-      return state.best;
-    }
-  }
-
-  void AddResult(AssemblerState &state) {
-    const auto& result = state.firstLetter;
-    if (state.best.length() == 0 || result.length() < state.best.length() ||
-        (result.length() == state.best.length() && result < state.best)) {
-      state.best = result;
-    }
-  }
-
-  void Truncate(std::string& strToTruncate, size_t amount){
-    strToTruncate.resize(strToTruncate.length() - amount);
-  }
-
-  void recursiveSearch(AssemblerState &state) {
-    auto actualPos = std::min(state.firstLetter.length(), state.secondLetter.length());
-    for (auto i = 0u; i < this->wordPairs.size(); ++i) {
-      if (!state.used[i]) {
-        state.used[i] = true;
-        const auto &firstWord = this->wordPairs[i].first;
-        const auto &secondWord = this->wordPairs[i].second;
-        state.firstLetter.append(firstWord);
-        state.secondLetter.append(secondWord);
-        auto endPos = std::min(state.firstLetter.length(), state.secondLetter.length());
-        auto isGoodContinuation =
-            std::equal(&state.firstLetter[actualPos], &state.firstLetter[endPos], &state.secondLetter[actualPos]);
-        if (isGoodContinuation) {
-          if (state.firstLetter.length() == state.secondLetter.length()) {
-            AddResult(state);
-          }
-          recursiveSearch(state);
-        }
-        Truncate(state.firstLetter, this->wordPairs[i].first.length());
-        Truncate(state.secondLetter, this->wordPairs[i].second.length());
-        state.used[i] = false;
-      }
-    }
-  }
+protected:
+  void recursiveSearch(AssemblerState &state);
 
   std::vector<std::pair<std::string, std::string>> wordPairs;
   size_t maximumPossibleLength;
 };
+
+void AddResult(LetterAssembler::AssemblerState &state);
+void Truncate(std::string &strToTruncate, size_t amount);
--- kattis/limited_correspondence/main.cpp
+++ kattis/limited_correspondence/main.cpp
@@ -1,4 +1,4 @@
-// https://open.kattis.com/submissions/4374599
+﻿// https://open.kattis.com/submissions/4374599
 
 #include "LetterAssembler.hpp"
 #include <fstream>
@@ -7,11 +7,10 @@
 int main() {
   auto caseNumber = 1u;
 
-  while (std::cin.good() == true) {
+  while (std::cin.good()) {
     LetterAssembler letterAssembler;
-    if (letterAssembler.read(std::cin)) {
-      std::cout << "Case " << caseNumber++ << ": " << letterAssembler.assemble() << std::endl;
-    }
+    letterAssembler.read(std::cin);
+    std::cout << "Case " << caseNumber++ << ": " << letterAssembler.assemble() << std::endl;
   }
   return 0;
 }
