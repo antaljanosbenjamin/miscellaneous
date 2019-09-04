4e8ee416f25885b25ec4095967383c843ce3c6d6
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2019-09-04 19:57:49 +0200

    Refactor TaskStealingTaskSystem

--- task_systems/TaskStealingTaskSystem.h
+++ task_systems/TaskStealingTaskSystem.h
@@ -9,17 +9,39 @@
 
 #include "NotificationQueue.h"
 
+template <size_t K>
 class TaskStealingTaskSystem {
   const unsigned count_{std::thread::hardware_concurrency()};
   std::vector<std::thread> threads_;
   std::vector<NotificationQueue> q_{count_};
   std::atomic<unsigned> index_{0};
 
-  void run(unsigned i);
+  void run(unsigned i) {
+    while (true) {
+      std::function<void()> f;
+      for (unsigned n = 0; n != count_ * K; ++n) {
+        if (q_[(i + n) % count_].try_pop(f))
+          break;
+      }
+      if (!f && !q_[i].pop(f))
+        break;
+      f();
+    }
+  }
 
 public:
-  TaskStealingTaskSystem();
-  ~TaskStealingTaskSystem();
+  TaskStealingTaskSystem() {
+    for (unsigned n = 0; n != count_; ++n) {
+      threads_.emplace_back([&, n] { run(n); });
+    }
+  }
+
+  ~TaskStealingTaskSystem() {
+    for (auto &e: q_)
+      e.done();
+    for (auto &e: threads_)
+      e.join();
+  }
 
   template <typename F> void async(F &&f) {
     auto i = index_++;
--- task_systems/main.cpp
+++ task_systems/main.cpp
@@ -8,11 +8,14 @@
 #include "OracleTaskSystem.h"
 #include "TaskStealingTaskSystem.h"
 
+#ifdef MY_DEBUG
 std::atomic<int> NOT_USED;
 std::atomic<int> task_count{0};
 std::mutex mutex;
+#endif
 
-template <typename TaskSystemType> void AddTasks(TaskSystemType &ts, std::vector<uint64_t> &runningTimes) {
+template <typename TaskSystemType>
+void AddTasks(TaskSystemType &ts, std::vector<uint64_t> &runningTimes) {
   for (auto runningTime: runningTimes) {
     ts.async([runningTimeInNs = runningTime]() {
       auto start = std::chrono::steady_clock::now();
@@ -26,12 +29,10 @@ template <typename TaskSystemType> void AddTasks(TaskSystemType &ts, std::vector
         result *= runningTimeInNs;
         result %= 73;
       }
-      // {
-      //   std::unique_lock<std::mutex> l(mutex);
-      //   std::cout << "Running time: |" << runningTimeInNs << "|\n";
-      // }
+#ifdef MY_DEBUG
       NOT_USED.fetch_add(result, std::memory_order::memory_order_relaxed);
       task_count.fetch_add(1, std::memory_order::memory_order_relaxed);
+#endif
     });
   }
 }
@@ -72,11 +73,20 @@ int main(int argc, char **argv) {
   } else if (testCase == 1u) {
     elapsedTime = MeasureTaskSystem<MultiQueuedTaskSystem>(runningTimes);
   } else if (testCase == 2u) {
-    elapsedTime = MeasureTaskSystem<TaskStealingTaskSystem>(runningTimes);
+    elapsedTime = MeasureTaskSystem<TaskStealingTaskSystem<1>>(runningTimes);
+  } else if (testCase == 3u) {
+    elapsedTime = MeasureTaskSystem<TaskStealingTaskSystem<5>>(runningTimes);
+  } else if (testCase == 4u) {
+    elapsedTime = MeasureTaskSystem<TaskStealingTaskSystem<10>>(runningTimes);
+  } else if (testCase == 5u) {
+    elapsedTime = MeasureTaskSystem<TaskStealingTaskSystem<50>>(runningTimes);
+  } else if (testCase == 6u) {
+    elapsedTime = MeasureTaskSystem<TaskStealingTaskSystem<100>>(runningTimes);
   }
+#ifdef MY_DEBUG
   std::cout << "NOT_USED: " << NOT_USED << "\n";
-
   std::cout << "Runned tasks: " << task_count << "\n";
+#endif
 
   std::cout << "Running time: " << std::chrono::duration_cast<std::chrono::milliseconds>(elapsedTime).count() << "ms\n";
 };
