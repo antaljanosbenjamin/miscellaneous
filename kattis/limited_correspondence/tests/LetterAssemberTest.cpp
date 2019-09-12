81dbe703adf7e8b841363a10799b9e3c7c81be72
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2019-09-12 22:46:31 +0200

    Add test for limited_correspondence

---
+++ kattis/limited_correspondence/tests/LetterAssemberTest.cpp
@@ -0,0 +1,46 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <sstream>
+#include "LetterAssembler.hpp"
+
+TEST_CASE("Empty stream") {
+  LetterAssembler assembler;
+  std::stringstream ss;
+  REQUIRE_FALSE(assembler.read(ss));
+}
+
+TEST_CASE("Example input") {
+  auto input = R"(5
+are yo
+you u
+how nhoware
+alan arala
+dear de
+8
+i ie
+ing ding
+resp orres
+ond pon
+oyc y
+hello hi
+enj njo
+or c
+3
+efgh efgh
+d cd
+abc ab
+3
+a ab
+b bb
+c cc)";
+  std::stringstream ss;
+  ss << input;
+  auto results = std::array<std::string, 4>{"dearalanhowareyou", "ienjoycorresponding", "abcd", "IMPOSSIBLE"};
+  LetterAssembler assembler;
+
+  for (const auto &expectedResult: results) {
+    REQUIRE(assembler.read(ss));
+    CHECK(assembler.assemble() == expectedResult);
+  }
+  REQUIRE_FALSE(assembler.read(ss));
+}
