5dc77050c68a52706692c66d2d6c331952e7b412
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-07-05 23:44:23 +0200

    Format Warnings.cmake

--- CMake/Warnings.cmake
+++ CMake/Warnings.cmake
@@ -1,15 +1,21 @@
 function(set_project_warnings project_name)
-  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
+  option(
+    WARNINGS_AS_ERRORS
+    "Treat compiler warnings as errors"
+    TRUE
+  )
 
   if(MSVC)
     target_compile_options(${project_name} INTERFACE /W4)
     if(WARNINGS_AS_ERRORS)
-      if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang"
-         AND "x${CMAKE_CXX_SIMULATE_ID}" STREQUAL "xMSVC"
+      if(${CMAKE_CXX_COMPILER_ID}
+         STREQUAL
+         "Clang"
+         AND "x${CMAKE_CXX_SIMULATE_ID}"
+             STREQUAL
+             "xMSVC"
       )
-        message(
-          FATAL_ERROR "Cannot use warnings as errors with Clang on Windows!"
-        )
+        message(FATAL_ERROR "Cannot use warnings as errors with Clang on Windows!")
       endif()
       target_compile_options(${project_name} INTERFACE /WX)
 
@@ -25,10 +31,11 @@ function(set_project_warnings project_name)
     if(WARNINGS_AS_ERRORS)
       target_compile_options(${project_name} INTERFACE -Werror)
 
-      if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
-        target_compile_options(
-          ${project_name} INTERFACE -Wno-unused-lambda-capture
-        )
+      if(${CMAKE_CXX_COMPILER_ID}
+         STREQUAL
+         "Clang"
+      )
+        target_compile_options(${project_name} INTERFACE -Wno-unused-lambda-capture)
       endif()
     endif()
   endif()
