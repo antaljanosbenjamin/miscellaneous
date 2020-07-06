b9493c04286223d8f75975f4fd44813d3ec7f1f2
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-07-07 01:06:44 +0200

    Get rid of custom before-install in case of CppCheck

--- travis/before_install/linux.sh
+++ travis/before_install/linux.sh
@@ -2,3 +2,7 @@
 echo "Removing CMake installed from source from PATH..."
 PATH=`echo $PATH | sed -re 's/\/usr\/local\/cmake-[[:digit:]]\.[[:digit:]][[:digit:]]\.[[:digit:]]+\/bin//'`
 echo "The new value of PATH: $PATH"
+
+if [ "$INSTALL_CPPCHECK" = true ] ; then
+  ./install_cppcheck.sh
+fi
