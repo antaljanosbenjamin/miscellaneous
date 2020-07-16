4568bb9cfceda4482a4064c3e5dc18c1c81f46a8
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-07-16 09:52:29 +0200

    add tests for cppminesweeper

---
+++ tests/minesweeper/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+#include "Minesweeper.hpp"
+
+int main() {
+  CErrorInfo errorInfo{};
+  constexpr auto maxErrorMessageLength = 100U;
+  std::vector<char> errorMessage(maxErrorMessageLength, 0);
+  errorInfo.errorMessage = errorMessage.data();
+  errorInfo.errorMessageMaxLength = errorMessage.size();
+  GameHandle gameHandle{};
+  minesweeper_new_game(&gameHandle, GameLevel::Expert, &errorInfo);
+  std::cout << "Error message: \"" << std::string(errorInfo.errorMessage)
+            << "\", error code: " << static_cast<uint32_t>(errorInfo.errorCode) << '\n';
+  if (errorInfo.errorCode != CError::Ok) {
+    return -1;
+  }
+
+  COpenInfo openInfo{};
+  constexpr auto fieldInfosLength = 100U;
+  std::vector<CFieldInfo> fieldInfos(fieldInfosLength, CFieldInfo{});
+  openInfo.fieldInfos = fieldInfos.data();
+  openInfo.fieldInfosLength = 0U;
+  openInfo.fieldInfosMaxLength = fieldInfos.size();
+
+  minesweeper_game_open(gameHandle, 0U, 0U, &openInfo, &errorInfo);
+  std::cout << "Error message: \"" << std::string(errorInfo.errorMessage)
+            << "\", error code: " << static_cast<uint32_t>(errorInfo.errorCode) << '\n';
+  if (errorInfo.errorCode != CError::Ok) {
+    return -2;
+  }
+  std::cout << openInfo.fieldInfosLength << '\n';
+
+  minesweeper_destroy_game(gameHandle);
+
+  return 0;
+}
