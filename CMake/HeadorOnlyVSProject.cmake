56f6bf7a30eabd68176a09f12f7b0dcd50ab3c1c
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-07-17 09:03:34 +0200

    Fix setting the FOLDER property for dummy projects of header only libraries

--- CMake/HeadorOnlyVSProject.cmake
+++ CMake/HeadorOnlyVSProject.cmake
@@ -2,11 +2,10 @@ function(add_dummy_project_for_header_only_lib DUMMY_TARGET_NAME LIB_HEADERS)
   cmake_parse_arguments(
     PARSED_ARGS
     ""
-    "LIB_FOLDER"
+    "FOLDER"
     ""
     ${ARGN}
   )
-  set(LIB_FOLDER ${PARSED_ARGS_LIB_FOLDER})
   # Dummy project to show the header-only lib in VS
   add_custom_target(
     ${DUMMY_TARGET_NAME}
@@ -14,9 +13,9 @@ function(add_dummy_project_for_header_only_lib DUMMY_TARGET_NAME LIB_HEADERS)
     SOURCES ${LIB_HEADERS}
   )
 
-  if(${LIB_FOLDER})
-    set_target_properties(${DUMMY_TARGET_NAME} PROPERTIES FOLDER ${LIB_FOLDER})
-    message(STATUS "FOLDER property of ${DUMMY_TARGET_NAME} is to ${LIB_FOLDER}")
+  if(PARSED_ARGS_FOLDER)
+    message(STATUS "FOLDER property of ${DUMMY_TARGET_NAME} is to ${PARSED_ARGS_FOLDER}")
+    set_target_properties(${DUMMY_TARGET_NAME} PROPERTIES FOLDER ${PARSED_ARGS_FOLDER})
   endif()
 
 endfunction()
