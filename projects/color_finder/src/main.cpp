77053a7120cf4aafc0eced4ded80c5141155cc3f
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-09-11 23:10:57 +0200

    Add static_assert to check evaluated value

--- projects/color_finder/src/main.cpp
+++ projects/color_finder/src/main.cpp
@@ -6,6 +6,7 @@
 int main() {
   constexpr ColorFinder::Color c{0x82, 0x12, 0x18};
   constexpr auto name = getClosestColorName(c);
+  static_assert(name == "Falu Red", "Something went wrong");
   std::cout << name << std::endl;
   return 0;
 }
