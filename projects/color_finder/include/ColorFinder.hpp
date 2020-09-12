bd4fda7a1d98d51abc7f8fac95ceb01f3c92eab0
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-09-13 01:13:56 +0200

    Make constexpr compiler dependant

--- projects/color_finder/include/ColorFinder.hpp
+++ projects/color_finder/include/ColorFinder.hpp
@@ -2,16 +2,17 @@
 
 #include <cmath>
 #include <vector>
+
+#include "ColorFinderConstexpr.h"
 #include "Colors.hpp"
 
 namespace ColorFinder {
 
-constexpr int sumOfDifference(const Color &lhs, const Color &rhs) {
-  const auto absValue = [](const int value) { return value < 0 ? -value : value; };
-  return absValue(lhs.r - rhs.r) + absValue(lhs.g - rhs.g) + absValue(lhs.b - rhs.b);
-}
-
-constexpr std::string_view getClosestColorName(const Color &color) {
+CF_CONSTEXPR std::string_view getClosestColorName(const Color &color) {
+  const auto sumOfDifference = [](const Color &lhs, const Color &rhs) {
+    const auto absValue = [](const int value) { return value < 0 ? -value : value; };
+    return absValue(lhs.r - rhs.r) + absValue(lhs.g - rhs.g) + absValue(lhs.b - rhs.b);
+  };
   int smallestDifference = sumOfDifference(color, colors[0].color);
   std::string_view closestColorName = colors[0].name;
   for (const auto &colorInfo: colors) {
--- projects/color_finder/src/main.cpp
+++ projects/color_finder/src/main.cpp
@@ -1,12 +1,19 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 
 #include "ColorFinder.hpp"
 
-int main() {
-  constexpr ColorFinder::Color c{0x82, 0x12, 0x18};
-  constexpr auto name = getClosestColorName(c);
-  static_assert(name == "Falu Red", "Something went wrong");
-  std::cout << name << std::endl;
+int main(const int argc, const char **argv) {
+  const auto getColorComponent = [](const char *str) {
+    return static_cast<uint8_t>(std::clamp(std::stoi(str), 0, 255));
+  };
+  if (argc != 4) {
+    std::cerr << "Please provide the three components in range [0,255]!\n";
+    return 1;
+  }
+
+  ColorFinder::Color c{getColorComponent(argv[1]), getColorComponent(argv[2]), getColorComponent(argv[3])};
+  std::cout << getClosestColorName(c) << std::endl;
   return 0;
 }
