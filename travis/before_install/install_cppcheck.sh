33cb998151eed883d5d77a9b0d29d3f521ec3ed7
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-09-13 22:58:10 +0200

    Remove invalid compiler argument

--- travis/before_install/install_cppcheck.sh
+++ travis/before_install/install_cppcheck.sh
@@ -6,7 +6,7 @@ cd /tmp/cppcheck
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make MATCHCOMPILER=yes FILESDIR=/usr/share/cppcheck HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function -Wno-inline" -j
+make MATCHCOMPILER=yes FILESDIR=/usr/share/cppcheck HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" -j
 sudo make -s install
 cppcheck --version
 cd $PREVIOUS_DIR
