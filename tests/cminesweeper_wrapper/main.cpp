95f7c3f13e151c506dd1ab735f57dc62876275a0
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-09-15 22:25:02 +0200

    Update cminesweeper_wrapper tests

--- tests/cminesweeper_wrapper/main.cpp
+++ tests/cminesweeper_wrapper/main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <vector>
 
-#include "Minesweeper.hpp"
+#include "CMinesweeper.hpp"
 
 int main() {
-  CErrorInfo errorInfo{};
+  CMErrorInfo errorInfo{};
   constexpr auto maxErrorMessageLength = 100U;
   std::vector<char> errorMessage(maxErrorMessageLength, 0);
   errorInfo.errorMessage = errorMessage.data();
   errorInfo.errorMessageMaxLength = errorMessage.size();
-  GameHandle gameHandle{};
-  minesweeper_new_game(&gameHandle, GameLevel::Expert, &errorInfo);
+  CMGameHandle gameHandle{};
+  minesweeper_new_game(&gameHandle, CMGameLevel::CMGL_Expert, &errorInfo);
   std::cout << "Error message: \"" << std::string(errorInfo.errorMessage)
             << "\", error code: " << static_cast<uint32_t>(errorInfo.errorCode) << '\n';
-  if (errorInfo.errorCode != CError::Ok) {
+  if (errorInfo.errorCode != CMError::CME_Ok) {
     return -1;
   }
 
-  COpenInfo openInfo{};
+  CMOpenInfo openInfo{};
   constexpr auto fieldInfosLength = 100U;
-  std::vector<CFieldInfo> fieldInfos(fieldInfosLength, CFieldInfo{});
+  std::vector<CMFieldInfo> fieldInfos(fieldInfosLength, CMFieldInfo{});
   openInfo.fieldInfos = fieldInfos.data();
   openInfo.fieldInfosLength = 0U;
   openInfo.fieldInfosMaxLength = fieldInfos.size();
@@ -27,12 +27,12 @@ int main() {
   minesweeper_game_open(gameHandle, 0U, 0U, &openInfo, &errorInfo);
   std::cout << "Error message: \"" << std::string(errorInfo.errorMessage)
             << "\", error code: " << static_cast<uint32_t>(errorInfo.errorCode) << '\n';
-  if (errorInfo.errorCode != CError::Ok) {
+  if (errorInfo.errorCode != CMError::CME_Ok) {
     return -2;
   }
   std::cout << openInfo.fieldInfosLength << '\n';
 
-  minesweeper_destroy_game(gameHandle);
+  minesweeper_destroy_game(&gameHandle, &errorInfo);
 
   return 0;
 }
