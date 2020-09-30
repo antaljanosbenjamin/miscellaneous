c9d10ba97267d954bdd0070d823d9bf6fa268294
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-09-30 21:19:21 +0200

    Drop C support for CMinesweeper

--- projects/cminesweeper_wrapper/include/CMinesweeper.hpp
+++ projects/cminesweeper_wrapper/include/CMinesweeper.hpp
@@ -1,77 +1,74 @@
 #pragma once
 
-#ifdef __cplusplus
+#include <cstdint>
+
 extern "C" {
-#endif
 
-#include <stdint.h>
-
-typedef enum CMError : uint32_t {
+enum CMError : uint32_t {
   CME_Ok,
   CME_InvalidInput,
   CME_NullPointerAsInput,
   CME_IndexIsOutOfRange,
   CME_InsufficientBuffer,
   CME_UnexpectedError,
-} CMError;
+};
 
-typedef enum CMFieldTypeTag : uint32_t {
+enum CMFieldTypeTag : uint32_t {
   CMFTT_Empty,
   CMFTT_Numbered,
   CMFTT_Mine,
-} CMFieldTypeTag;
+};
 
-typedef struct CMFieldTagNumberedPayload {
+struct CMFieldTagNumberedPayload {
   uint8_t value;
-} CMFieldTagNumberedPayload;
+};
 
-typedef struct CMFieldType {
-  CMFieldTypeTag tag;
-  CMFieldTagNumberedPayload payload;
-} CMFieldType;
+struct CMFieldType {
+  CMFieldTypeTag tag{CMFieldTypeTag::CMFTT_Empty};
+  CMFieldTagNumberedPayload payload{0U};
+};
 
-typedef struct CMFieldInfo {
-  uint64_t row;
-  uint64_t col;
-  CMFieldType fieldType;
-} CMFieldInfo;
+struct CMFieldInfo {
+  uint64_t row{0U};
+  uint64_t col{0U};
+  CMFieldType fieldType{};
+};
 
-typedef enum CMOpenResult {
+enum CMOpenResult {
   CMOR_Ok,
   CMOR_IsFlagged,
   CMOR_Boom,
   CMOR_Winner,
-} CMOpenResult;
+};
 
-typedef struct CMOpenInfo {
-  CMOpenResult result;
-  uint64_t fieldInfosLength;
-  uint64_t fieldInfosMaxLength;
-  CMFieldInfo *fieldInfos;
-} CMOpenInfo;
+struct CMOpenInfo {
+  CMOpenResult result{CMOR_Ok};
+  uint64_t fieldInfosLength{0U};
+  uint64_t fieldInfosMaxLength{0U};
+  CMFieldInfo *fieldInfos{nullptr};
+};
 
 struct CMErrorInfo {
-  CMError errorCode;
+  CMError errorCode{CMError::CME_UnexpectedError};
   uint64_t errorMessageLength{0U};
   uint64_t errorMessageMaxLength{0U};
   char *errorMessage{nullptr};
 };
 
-typedef enum CMGameLevel {
+enum CMGameLevel {
   CMGL_Beginner,
   CMGL_Intermediate,
   CMGL_Expert,
-} CMGameLevel;
+};
 
-typedef enum CMFieldFlagResult {
+enum CMFieldFlagResult {
   CMFFR_Flagged,
   CMFFR_FlagRemoved,
   CMFFR_AlreadyOpened,
-} CMFieldFlagResult;
+};
 
-typedef struct CMGame CMGame;
-typedef CMGame *CMGameHandle;
-typedef const CMGame *ConstCMGameHandle;
+using CMGameHandle = struct CMGame *;
+using ConstCMGameHandle = const CMGame *;
 
 void minesweeper_new_game(CMGameHandle *gameHandle, CMGameLevel level, CMErrorInfo *errorInfo);
 void minesweeper_game_open(CMGameHandle gameHandle, const uint64_t row, const uint64_t column, CMOpenInfo *openInfo,
