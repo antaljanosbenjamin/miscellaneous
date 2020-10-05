302ee3dcc18d4ff3a5ae7525f4486003256a1b38
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-10-05 23:33:50 +0200

    Indent ifdefs

--- projects/color_finder/include/ColorFinderConstexpr.h
+++ projects/color_finder/include/ColorFinderConstexpr.h
@@ -3,11 +3,11 @@
 namespace ColorFiner {
 
 #if defined(TI_IS_MSVC) || defined(TI_IS_CLANG_CL) || defined(TI_IS_CLANG)
-#define CF_CONSTEXPR constexpr // NOLINT(cppcoreguidelines-macro-usage)
-#define CF_USE_CONSTEXPR true  // NOLINT(cppcoreguidelines-macro-usage)
+  #define CF_CONSTEXPR constexpr // NOLINT(cppcoreguidelines-macro-usage)
+  #define CF_USE_CONSTEXPR true  // NOLINT(cppcoreguidelines-macro-usage)
 #else
-#define CF_CONSTEXPR
-#define CF_USE_CONSTEXPR false // NOLINT(cppcoreguidelines-macro-usage)
+  #define CF_CONSTEXPR
+  #define CF_USE_CONSTEXPR false // NOLINT(cppcoreguidelines-macro-usage)
 #endif
 
 constexpr bool useConstexpr = CF_USE_CONSTEXPR;
