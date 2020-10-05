6bbe0f91adbe6a05caab59dd6d6d2b039b4c99e5
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-10-05 23:34:27 +0200

    Add CONAN_OPTIONS

--- CMake/Conan.cmake
+++ CMake/Conan.cmake
@@ -13,6 +13,7 @@ macro(run_conan)
 
   conan_add_remote(NAME bincrafters URL https://api.bintray.com/conan/bincrafters/public-conan)
   set(CONAN_DEPENDENCIES catch2/2.11.0 tl-expected/20190710)
+  set(CONAN_OPTIONS "")
 
   if(WIN32)
     list(APPEND CONAN_DEPENDENCIES propagate_const/1.0.0@public-conan/testing)
@@ -21,6 +22,7 @@ macro(run_conan)
 
   if(NOT TI_IS_CLANG_CL)
     list(APPEND CONAN_DEPENDENCIES wxwidgets/3.1.2@bincrafters/stable)
+    list(APPEND CONAN_OPTIONS wxwidgets:compatibility=3.1 wxwidgets:debugreport=False)
   endif()
 
   conan_cmake_run(
@@ -30,5 +32,7 @@ macro(run_conan)
     CMAKE_TARGETS # individual targets to link to
     BUILD
     missing
+    OPTIONS
+    ${CONAN_OPTIONS}
   )
 endmacro()
