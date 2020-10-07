62309a1b31efa2fd0e49b2de0f6fe72ac0b95f9b
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-10-07 23:29:19 +0200

    Update to use panels instead of buttons

--- projects/minesweeper/minesweeper_gui/include/WxWidgetsWrapper.hpp
+++ projects/minesweeper/minesweeper_gui/include/WxWidgetsWrapper.hpp
@@ -5,6 +5,7 @@ PRAGMA_WARNING_IGNORE_MSVC(WARNING_MSVC_THIS_FUNCTION_OR_VARIABLE_MAY_BE_UNSAGE)
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
   #include <wx/artprov.h>
+  #include <wx/dcbuffer.h>
   #include <wx/wx.h>
 #endif
 PRAGMA_WARNING_POP_MSVC()
