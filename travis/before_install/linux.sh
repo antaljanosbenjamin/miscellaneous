6dab9a93464ece5a049cf38ad06f1245915ba80a
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2021-01-13 23:25:43 +0100

    Delete default conan profile after build

--- travis/before_install/linux.sh
+++ travis/before_install/linux.sh
@@ -6,3 +6,10 @@ echo "The new value of PATH: $PATH"
 if [ "$INSTALL_CPPCHECK" = true ] ; then
   travis/before_install/install_cppcheck.sh
 fi
+
+if [ -f "$HOME/.conan/profiles/default" ]; then
+    echo "Default profile exists, deleting it."
+    rm -f "$HOME/.conan/profiles/default"
+else 
+    echo "Default profile doesn't exist"
+fi
