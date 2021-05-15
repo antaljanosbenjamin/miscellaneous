597f6b5be45544fd67d551db7e5132f869864bc8
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2021-05-15 10:44:32 +0200

    Move main to the global namespace

--- projects/minesweeper/minesweeper_gui/src/main.cpp
+++ projects/minesweeper/minesweeper_gui/src/main.cpp
@@ -3,7 +3,6 @@
 #include "FieldsFrame.hpp"
 #include "WxWidgetsWrapper.hpp"
 
-namespace minesweeper_gui {
 class MyApp : public wxApp {
 public:
   bool OnInit() override;
@@ -11,14 +10,12 @@ public:
 
 wxIMPLEMENT_APP(MyApp); // NOLINT(cert-err58-cpp, cppcoreguidelines-pro-type-static-cast-downcast)
 
-std::unique_ptr<FieldsFrame> frame;
+std::unique_ptr<minesweeper_gui::FieldsFrame> frame;
 
 bool MyApp::OnInit() {
   wxImage::AddHandler(new wxPNGHandler{}); // NOLINT(cppcoreguidelines-owning-memory)
-  frame = std::make_unique<FieldsFrame>();
+  frame = std::make_unique<minesweeper_gui::FieldsFrame>();
   frame->Show(true);
   static_cast<void>(frame.release());
   return true;
 }
-
-} // namespace minesweeper_gui
