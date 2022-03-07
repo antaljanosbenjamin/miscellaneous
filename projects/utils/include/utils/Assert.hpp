2bafdcaabaae35b218ff79e44147ed71391ffe66
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2022-03-07 23:12:22 +0100

    Use C++ likely

--- projects/utils/include/utils/Assert.hpp
+++ projects/utils/include/utils/Assert.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "utils/Likely.hpp"
-
 namespace utils::detail::assert {
 [[noreturn]] void HandleFailedAssert(const char *filename, int lineNumber, const char *expresssion,
                                      const char *message);
@@ -9,5 +7,5 @@ namespace utils::detail::assert {
 
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define MY_ASSERT(expression, message)                                                                                 \
-  MY_LIKELY(!!(expression)) ? static_cast<void>(0)                                                                     \
-                            : utils::detail::assert::HandleFailedAssert(__FILE__, __LINE__, #expression, message);
+  [[likely]] (!!(expression)) ? static_cast<void>(0)                                                                   \
+                              : utils::detail::assert::HandleFailedAssert(__FILE__, __LINE__, #expression, message);
