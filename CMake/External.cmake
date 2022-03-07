88a079c08efb084bb7e7095f67dc4dd20cb791ff
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2022-03-07 23:50:31 +0100

    Use latest conan

--- CMake/External.cmake
+++ CMake/External.cmake
@@ -4,7 +4,7 @@ macro(run_conan)
   # Download automatically, you can also just copy the conan.cmake  file
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/develop/conan.cmake"
          "${CMAKE_BINARY_DIR}/conan.cmake"
     )
   endif()
