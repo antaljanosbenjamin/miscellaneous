03ea237e0d3a8387f21625890ae86172fccdec48
János Benjamin Antal <janos.benjamin.antal@gmail.com>
2022-07-01 23:38:39 +0200

    Add first version of algorithm

--- projects/matrix_connected_components/src/Algorithm.cpp
+++ projects/matrix_connected_components/src/Algorithm.cpp
@@ -1,22 +1,93 @@
 #include "Algorithm.hpp"
 
+#include <algorithm>
+#include <chrono>
 #include <cstdint>
+#include <iostream>
+#include <limits>
+#include <type_traits>
+#include <unordered_set>
 
 #include "MatrixSlice.hpp"
+#include "utils/DisjointSets.hpp"
 
 namespace matrix_connected_components {
 
 void doMagic(const utils::Matrix<uint8_t> &matrix) {
-  auto labelledMatrix = utils::Matrix<uint64_t>::sameSizeAs(matrix);
 
-  if (labelledMatrix.width() != matrix.width()) {
-    throw 'w';
+  using LabelType = int64_t;
+  static constexpr LabelType kUnmarked = 0;
+  static constexpr LabelType kMarked = 1;
+  static constexpr LabelType kFirstLabel = kMarked + 1;
+
+  utils::Matrix<LabelType> labelledMatrix{matrix.height() + 2, matrix.width() + 2, kUnmarked};
+
+  for (auto row = 0L; row < matrix.height(); ++row) {
+    for (auto column = 0L; column < matrix.width(); ++column) {
+      if (matrix.get(row, column) != 0) {
+        labelledMatrix.get(row + 1, column + 1) = kMarked;
+      }
+    }
   }
-  if (labelledMatrix.height() != matrix.height()) {
-    throw 'h';
+
+  const auto firstRow = 1L;
+  const auto firstColumn = 1L;
+  const auto lastRow = labelledMatrix.height() - 2;
+  const auto lastColumn = labelledMatrix.width() - 2;
+
+  utils::DisjointSets labelUnions;
+  labelUnions.add(kUnmarked);
+  labelUnions.add(kMarked);
+  [[maybe_unused]] auto print = [&]() {
+    for (auto row = 0U; row < labelledMatrix.height(); ++row) {
+      for (auto column = 0U; column < labelledMatrix.width(); ++column) {
+        std::cout << static_cast<int>(*labelUnions.find(labelledMatrix.get(row, column))) << ' ';
+      }
+      std::cout << std::endl;
+    }
+  };
+  auto currentLabel = kFirstLabel;
+  for (auto row = firstRow; row <= lastRow; ++row) {
+    for (auto column = firstColumn; column <= lastColumn; ++column) {
+      auto &label = labelledMatrix.get(row, column);
+      if (label == kUnmarked) {
+        continue;
+      }
+      int64_t smallerLabel{kUnmarked};
+      int64_t greaterLabel{kUnmarked};
+      if (row > firstRow) {
+        smallerLabel = labelledMatrix.get(row - 1, column);
+      }
+      if (column > firstColumn) {
+        greaterLabel = labelledMatrix.get(row, column - 1);
+      }
+      if (greaterLabel < smallerLabel) {
+        std::swap(smallerLabel, greaterLabel);
+      }
+
+      if (smallerLabel == kUnmarked) {
+        // we don't have any collision for sure
+        if (greaterLabel == kUnmarked) {
+          // We need a new label
+          label = currentLabel++;
+
+          labelUnions.add(label);
+
+        } else {
+          label = greaterLabel;
+        }
+      } else {
+        // we might have a collision
+        label = smallerLabel;
+        if (smallerLabel != greaterLabel) {
+
+          labelUnions.merge(smallerLabel, greaterLabel);
+        }
+      }
+      // print();
+      // std::cout << "--------------------------\n";
+    }
   }
-  auto labelledMatrixSlice = MatrixSlice{labelledMatrix, 0, 0, 2, 4};
-  auto labelledMatrixSliceSlice = MatrixSlice{labelledMatrixSlice, 1, 1, 1, 2};
 }
 
 } // namespace matrix_connected_components
--- projects/matrix_connected_components/src/main.cpp
+++ projects/matrix_connected_components/src/main.cpp
@@ -1,26 +1,54 @@
 
 #include <iostream>
 
+#include <chrono>
+#include <random>
+
 #include "Algorithm.hpp"
 #include "utils/Matrix.hpp"
 
 int main() {
-  static constexpr size_t width{20};
-  static constexpr size_t height{10};
-  utils::Matrix<uint8_t> m{width, height, 2};
+  // static constexpr int64_t width{6};
+  // static constexpr int64_t height{5};
+  // utils::Matrix<uint8_t> m{height, width, 0};
 
-  for (auto row = 0U; row < height; ++row) {
-    for (auto column = 0U; column < width; ++column) {
-      m.get(row, column) = row * width + column;
-    }
-  }
-  for (auto row = 0U; row < height; ++row) {
-    for (auto column = 0U; column < width; ++column) {
-      std::cout << static_cast<int>(m.get(row, column)) << ' ';
-    }
-    std::cout << std::endl;
+  // std::vector<std::pair<int64_t, int64_t>> markedFields = {
+  //     {0, 0}, {1, 0}, {1, 2}, {1, 3}, {2, 1}, {2, 3}, {3, 1}, {3, 2}, {3, 3}, {4, 4},
+  // };
+
+  // for (const auto &[row, column]: markedFields) {
+  //   m.get(row, column) = 1;
+  // }
+  // for (auto row = 0U; row < height; ++row) {
+  //   for (auto column = 0U; column < width; ++column) {
+  //     std::cout << static_cast<int>(m.get(row, column)) << ' ';
+  //   }
+  //   std::cout << std::endl;
+  // }
+  // std::cout << std::endl;
+  // std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+  // matrix_connected_components::doMagic(m, begin);
+
+  static constexpr int64_t kDimension{20000};
+  static constexpr int64_t kNumberOfFields{kDimension * kDimension};
+
+  std::mt19937 mt(1); // NOLINTNEXTLINE(cert-msc32-c)
+  std::uniform_int_distribution<int64_t> r1(0, kDimension - 1);
+  utils::Matrix<uint8_t> matrix(kDimension, kDimension);
+
+  static constexpr int64_t kIterations = kNumberOfFields / 5 * 2;
+  for (auto i = 0; i < kIterations; ++i) {
+    matrix.get(r1(mt), r1(mt)) = 1;
   }
 
-  matrix_connected_components::doMagic(m);
+  std::cout << "Start\n";
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+  matrix_connected_components::doMagic(matrix);
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  std::cout << "Done\n";
+
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
+            << "[ms]" << std::endl;
+
   return 0;
 }
