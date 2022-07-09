971781ddcb272eae9f727953876f824286527fe5
János Benjamin Antal <janos.benjamin.antal@gmail.com>
2022-07-09 18:48:57 +0200

    Add new functions to DisjointSet

--- projects/utils/include/utils/containers/DisjointSet.hpp
+++ projects/utils/include/utils/containers/DisjointSet.hpp
@@ -18,6 +18,16 @@ public:
     return static_cast<int64_t>(m_parents.size());
   }
 
+  [[nodiscard]] int64_t numberOfDisjointSets() const {
+    int64_t counter{0};
+    for (const auto &[value, parent]: this->m_parents) {
+      if (value == parent) {
+        ++counter;
+      }
+    }
+    return counter;
+  }
+
   bool add(const ValueType value) {
     if (m_parents.contains(value)) {
       return false;
@@ -26,7 +36,14 @@ public:
     return true;
   }
 
-  std::optional<ValueType> find(const ValueType value) {
+  [[nodiscard]] std::optional<ValueType> find(const ValueType value) {
+    if (auto it = m_parents.find(value); it != m_parents.end()) {
+      return this->findParent(it)->first;
+    }
+    return std::nullopt;
+  }
+
+  [[nodiscard]] std::optional<ValueType> find(const ValueType value) const {
     if (auto it = m_parents.find(value); it != m_parents.end()) {
       return this->findParent(it)->first;
     }
@@ -54,8 +71,10 @@ public:
 
     if (lhsParent->first < rhsParent->first) {
       rhsParent->second = lhsParent->first;
+      rhsIts->second = lhsParent->first;
     } else {
       lhsParent->second = rhsParent->first;
+      lhsIts->second = rhsParent->first;
     }
     return true;
   }
@@ -63,13 +82,22 @@ public:
 private:
   using HashMap = std::unordered_map<ValueType, ValueType>;
   using NodeIterator = typename HashMap::iterator;
-  [[nodiscard]] NodeIterator findParent(NodeIterator it) {
+  using ConstNodeIterator = typename HashMap::const_iterator;
 
+  [[nodiscard]] NodeIterator findParent(NodeIterator it) {
     auto originalIt = it;
     while (it->first != it->second) {
       it = m_parents.find(it->second);
     }
     originalIt->second = it->second;
+    return it;
+  }
+
+  [[nodiscard]] ConstNodeIterator findParent(ConstNodeIterator it) const {
+    while (it->first != it->second) {
+      it = m_parents.find(it->second);
+    }
+
     return it;
   }
 
